edfa057395493ab818461ac1ab29e191ef681862
irfansm07 <smirfan9247@gmail.com>
2025-10-25 16:31:46 +0530

    Update vibemap.js

--- vibemap.js
+++ vibemap.js
@@ -1,6 +1,6 @@
 // VIBEXPERT - COMPLETE JAVASCRIPT WITH BACKEND INTEGRATION
 
-const API_URL = 'https://vibexpert-backend-main.onrender.com';
+const API_URL = 'http://vibexpert-backend-main.onrender.com';
 
 let currentUser = null;
 let currentType = null;
@@ -1087,5 +1087,6 @@ function msg(text, type) {
   box.appendChild(div);
   setTimeout(() => div.remove(), 3500);
 }
+
 
 
